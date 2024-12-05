43a3ce83d362d0cbb1e70bb62380ba6e253e2616
Davi Assunção dos Santos <82240058+davi011205@users.noreply.github.com>
2024-12-05 09:56:23 -0300

    realTime e exclusao de items

--- firebaseapp/src/pages/Home/index.js
+++ firebaseapp/src/pages/Home/index.js
@@ -1,14 +1,44 @@
+import { useState, useEffect } from 'react';
 import { db } from "../../firebaseConfig";
-import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore'
-import { useState } from 'react'
+import { 
+    doc, 
+    setDoc, 
+    collection, 
+    addDoc, 
+    getDoc, 
+    getDocs, 
+    updateDoc, 
+    deleteDoc, 
+    onSnapshot
+} from 'firebase/firestore';
+
 import { toast } from "react-toastify";
 
 function Home() {
     const [fields, setFields] = useState({});
     const [users, setUsers] = useState([]);
 
+    useEffect(() => {
+       async function loadUsers() { //um snapshot que verifica em tempo real a coleção de users
+        const realTime = onSnapshot(collection(db, 'users'), (snapshot) => {
+            let listaUsers = [];
+
+                snapshot.forEach((doc) => {
+                    listaUsers.push({
+                        id: doc.id,
+                        nome: doc.data().nome,
+                        idade: doc.data().idade
+                    })
+                })
+                setUsers(listaUsers)
+        })
+       }
+       loadUsers()
+    }, [])
+
     const handleChange = (e) => {
         setFields({...fields, [e.target.name]: e.target.value});
+        console.log(setFields);
     }
     
     async function handleAdd(e) {
@@ -61,31 +91,73 @@ function Home() {
                     })
                 })
                 setUsers(lista)
+                
             })
-        .catch((error) => {
+        .catch(() => {
             toast.error('nao foi possivel carregar os usuários')
         })
+    }
+
+
+    async function editarUsuario() {
+        const docRef = doc(db, 'users', fields.idUser)
+        await updateDoc(docRef, {
+            idade: fields.idade,
+            nome: fields.nome
+        })
+        .then(() => {
+            toast.success('usuario atualizado com sucesso')
+            setFields({nome: '', idade: '', idUser: ''})
+       
+        })
+        .catch(() => {
+            toast.error('nao foi possivel atualizar')
+        })
+
+    }
+
+    async function excluirUsuario(id) {
+        await deleteDoc(doc(db, 'users', id))
+        .then(() => {
+            toast.success('usuario excluido com sucesso')
+        })
+        .catch(() => {
+            toast.error('nao foi possível excluir o usuario')
+        })
+
     }
 
     return(
         <div>
             <h1>Cadastre um novo usuario</h1>
-            <form onSubmit={handleAdd}>
+     
+                Id do Usuario
+                {/* <input type='text' name="idUser" value={fields.idUser} onChange={handleChange}></input> <br/><br/> */}
+                <select name="idUser" value={fields.idUser} onChange={handleChange}>
+                    <option>selecione um usuario</option>
+                    {users.map((user) => {
+                        return(
+                            <option key={user.id}>{user.id}</option>
+                        )
+                    })}
+                </select>
+                <br/><br/>
                 Nome
-                <input type='text' name="nome" value={fields.nome} onChange={handleChange}></input>
+                <input type='text' name="nome" value={fields.nome} onChange={handleChange}></input> <br/><br/>
                 Idade
-                <input type='number' name="idade" value={fields.idade} onChange={handleChange}></input>
+                <input type='number' name="idade" value={fields.idade} onChange={handleChange}></input> <br/><br/>
 
-                <button type='submit'>Cadastrar</button>
-            </form>
-
-            <button onClick={buscarUsuario}>Buscar usuario</button>
+                <button onClick={handleAdd}>Cadastrar</button>
+                <button onClick={buscarUsuario}>Buscar usuario</button>
+                <button onClick={editarUsuario}>Editar usuario</button>
             <ul>
                 {users.map((user) => {
                     return(
                         <li key={user.id}>
+                            <span>{user.id}</span>
                             <h1>nome: {user.nome}</h1>
                             <h1>idade: {user.idade}</h1>
+                            <button onClick={ () => excluirUsuario(user.id) }>Excluir</button>
                         </li>
                     )
                 })}
