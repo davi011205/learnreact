92befe9ed287f1c689f37804c1d87a14c80190ed
Davi Assunção dos Santos <82240058+davi011205@users.noreply.github.com>
2024-12-17 10:17:50 -0300

    voltando ao antigo

--- projetos/primeflix/src/App.js
+++ projetos/primeflix/src/App.js
@@ -1,25 +1,24 @@
-import { BrowserRouter } from "react-router-dom";
 import RoutesApp from "./routes";
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
+    <>
+     <ToastContainer
+      position="top-center"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
       <RoutesApp></RoutesApp>
-    </BrowserRouter>
+    </>
   );
 }
 
