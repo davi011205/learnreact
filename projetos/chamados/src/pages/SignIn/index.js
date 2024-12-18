4f0822d92bc77dae36eebe89044730a1795e12ef
Davi Assunção dos Santos <82240058+davi011205@users.noreply.github.com>
2024-12-18 18:07:37 -0300

    tela de login
    design da pagina de login

--- projetos/chamados/src/pages/SignIn/index.js
+++ projetos/chamados/src/pages/SignIn/index.js
@@ -1,7 +1,50 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+
+import './signin.css';
+import logo from '../../assets/logo.png'
+
+
 function SignIn() {
+    const [fields, setFields] = useState({});
+    const [error, setError] = useState('');
+
+    const handleChange = (e) => {
+        setFields({...fields, [e.target.name]: e.target.value} )
+        
+    }
+
+    function loginUser(e) {
+        e.preventDefault()
+        console.log(fields)
+    }
+
     return(
-        <div>
-            <h1>pagina de login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt=''></img>
+                </div>
+
+            <form onSubmit={loginUser}>
+                <h1>Entrar</h1>
+
+                <input type='email'
+                    placeholder='Email'
+                    name='email'
+                    value={fields.email || ''}
+                    onChange={handleChange}
+                />
+                <input type='text'
+                    placeholder='******'
+                    name='password'
+                    value={fields.password || ''}
+                    onChange={handleChange}
+                />
+                <button type='submit'>Acessar</button>
+            </form>
+                <Link to='/register'>Não possui uma conta? <strong>Cadastre-se</strong></Link>
+            </div>
         </div>
     )
 }
--- projetos/chamados/src/pages/SignUp/index.js
+++ projetos/chamados/src/pages/SignUp/index.js
@@ -1,7 +1,51 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+
+import '../SignIn/signin.css';
+import logo from '../../assets/logo.png'
+
+
 function SignUp() {
+    const [fields, setFields] = useState({});
+    const [error, setError] = useState('');
+
+    const handleChange = (e) => {
+        setFields({...fields, [e.target.name]: e.target.value} )
+        
+    }
+
+    function loginUser(e) {
+        e.preventDefault()
+        console.log(fields)
+    }
+
     return(
-        <div>
-            <h1>pagina de cadastro</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt=''></img>
+                </div>
+
+                <form onSubmit={loginUser}>
+                    <h1>Entrar</h1>
+
+                    <input type='email'
+                        placeholder='Email'
+                        name='email'
+                        value={fields.email || ''}
+                        onChange={handleChange}
+                    />
+                    <input type='text'
+                        placeholder='******'
+                        name='password'
+                        value={fields.password || ''}
+                        onChange={handleChange}
+                    />
+                    <button type='submit'>Acessar</button>
+                </form>
+
+                <Link to='/'>Já possui uma conta? <strong>Entrar</strong></Link>
+            </div>
         </div>
     )
 }
